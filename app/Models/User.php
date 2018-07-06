39a973051d7769df974bb2676b52d2ac407f89df
vagrant <1969839713@example.com>
2018-07-06 07:10:15 +0000

    Move user model to models folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
-use Illuminate\Notifications\Notifiable;
+use Illuminate\NotiFFfications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
