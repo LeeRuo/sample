a5ac85d67a005fd7a188f93701131dae725cceec
vagrant <1969839713@example.com>
2018-07-06 11:23:22 +0000

    modeling-users

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\NotiFFfications\Notifiable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
